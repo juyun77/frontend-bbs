5749c759949eb47663eeb567302151304311949f
juyun <juyun571@gmail.com>
2024-01-30 14:26:25 +0900

    initial

---
+++ src/Components/app/Footer.js
@@ -0,0 +1,19 @@
+import "../../css/footer.css"
+
+function Footer() {
+	return (
+		<footer className="py-4 bg-dark text-light footer">
+          <div className="container text-center">
+            <ul className="nav justify-content-center mb-3">
+              <li className="nav-item">
+                <a className="nav-link text-light" href="/">Top</a>
+              </li>
+            </ul>
+
+         
+          </div>
+        </footer>
+	);
+}
+
+export default Footer;
---
+++ src/Components/app/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<header className="py-4">
+			<div className="container text-center">
+				<h1>Cloud 게시판</h1>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
